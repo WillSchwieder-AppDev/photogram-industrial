e4e5c2ee8069ecda02bd024d4e60e1796d229c84
WillSchwieder <wschwied@chicagobooth.edu>
2022-04-13 17:46:36 +0000

    ran the rails db to commit changes'

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,23 @@
 # Table name: users
 #
 #  id                     :bigint           not null, primary key
+#  comments_count         :integer          default(0)
 #  email                  :citext           default(""), not null
 #  encrypted_password     :string           default(""), not null
-#  reset_password_token   :string
-#  reset_password_sent_at :datetime
-#  remember_created_at    :datetime
-#  username               :citext
-#  private                :boolean
 #  likes_count            :integer          default(0)
-#  comments_count         :integer          default(0)
+#  private                :boolean
+#  remember_created_at    :datetime
+#  reset_password_sent_at :datetime
+#  reset_password_token   :string
+#  username               :citext
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_email                 (email) UNIQUE
+#  index_users_on_reset_password_token  (reset_password_token) UNIQUE
+#  index_users_on_username              (username) UNIQUE
 #
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
@@ -21,7 +27,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :photo, counter_cache = true
+  has_many :photo
   has_many :comments
   has_many :follow_requests
   has_many :like
